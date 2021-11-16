2a5dc9f5aa21d20f92c33fa2169cc87169bee8e5
manyy <manyy.openkt@gmail.com>
2021-11-16 21:12:23 +0800

    revert changes

--- skynet-src/skynet_harbor.c
+++ skynet-src/skynet_harbor.c
@@ -9,7 +9,7 @@
 #include <assert.h>
 
 static struct skynet_context * REMOTE = 0;
-static unsigned int HARBOR = ~~0;
+static unsigned int HARBOR = ~0;
 
 static inline int
 invalid_type(int type) {
@@ -24,8 +24,8 @@ skynet_harbor_send(struct remote_message *rmsg, uint32_t source, int session) {
 
 int 
 skynet_harbor_message_isremote(uint32_t handle) {
-	assert(HARBOR != ~~0);
-	int h = (handle & ~~HANDLE_MASK);
+	assert(HARBOR != ~0);
+	int h = (handle & ~HANDLE_MASK);
 	return h != HARBOR && h !=0;
 }
 
